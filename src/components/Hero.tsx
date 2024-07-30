7c20d643320a830754999738ecd72893a8d21872
sebbyrule <60407118+sebbyrule@users.noreply.github.com>
2024-07-30 16:12:10 -0400

    added escape char for apostrophe

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
       <div className="md:w-2/3">
         <h1 className="text-5xl font-bold mb-4">Welcome to SebDoesMedia</h1>
         <p className="text-xl">Master Programming, Maximize Productivity, and Discover Insightful Teachings</p>
-        <p>Welcome to SebDoesMedia, your go-to resource for transformative programming projects, insightful articles on effective learning and time management, and comprehensive book reviews. Whether you're a seasoned coder or just starting your journey, SebDoesMedia is here to inspire and guide you every step of the way.</p>
+        <p>Welcome to SebDoesMedia, your go-to resource for transformative programming projects, insightful articles on effective learning and time management, and comprehensive book reviews. Whether you&apos;re a seasoned coder or just starting your journey, SebDoesMedia is here to inspire and guide you every step of the way.</p>
       </div>
       <div className="md:w-1/3">
         <Image src="/SebDoesMedia.png" alt="SebDoesMedia" width={200} height={200} />
