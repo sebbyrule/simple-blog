515c11124cdffcc0a309baf4ac94ea9d40e37175
sebbyrule <60407118+sebbyrule@users.noreply.github.com>
2024-07-30 16:08:00 -0400

    profile picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import profile from '@/public/SebDoesMedia.png'
 const Hero: React.FC = () => {
   return (
     <div className="py-20 container mx-auto text-center flex flex-col md:flex-row items-center">
@@ -10,7 +9,7 @@ const Hero: React.FC = () => {
         <p>Welcome to SebDoesMedia, your go-to resource for transformative programming projects, insightful articles on effective learning and time management, and comprehensive book reviews. Whether you're a seasoned coder or just starting your journey, SebDoesMedia is here to inspire and guide you every step of the way.</p>
       </div>
       <div className="md:w-1/3">
-        <Image src={profile} alt="SebDoesMedia" width={200} height={200} />
+        <Image src="/SebDoesMedia.png" alt="SebDoesMedia" width={200} height={200} />
       </div>
     </div>
 
